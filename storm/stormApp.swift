5c0daf4af43811199345f01760112f725253cdb7
qwy16 <qwy@msn.com>
2025-07-09 15:58:38 +0800

    Initial Commit

---
+++ storm/stormApp.swift
@@ -0,0 +1,17 @@
+//
+//  stormApp.swift
+//  storm
+//
+//  Created by Wenyan Qin on 2025-07-09.
+//
+
+import SwiftUI
+
+@main
+struct stormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
